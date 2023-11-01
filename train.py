070fede9d671e54b6065c92f2728125fba5f8579
Kye <kye@apacmediasolutions.com>
2023-11-01 09:32:42 -0400

    reshape text sampler to 2dg

--- train.py
+++ train.py
@@ -47,15 +47,17 @@ class TextSamplerDataset(Dataset):
     def __init__(self, data, seq_len):
         super().__init__()
         self.data = data
-        self.seq_len = seq_len
+        self.side_len = int(np.sqrt(seq_len))  # Calculate side length of the square matrix
 
     def __getitem__(self, index):
-        rand_start = torch.randint(0, self.data.size(0) - self.seq_len, (1,))
-        full_seq = self.data[rand_start: rand_start + self.seq_len + 1].long()
-        return full_seq#cuda()
+        rand_start = torch.randint(0, self.data.size(0) - self.side_len**2, (1,))
+        seq = self.data[rand_start: rand_start + self.side_len**2].long()
+        matrix = seq.view(self.side_len, self.side_len)  # Reshape the sequence into a matrix
+        return matrix.cuda()
 
     def __len__(self):
-        return self.data.size(0) // self.seq_len
+        return self.data.size(0) // (self.side_len**2)
+
 
 train_dataset = TextSamplerDataset(data_train, SEQ_LEN)
 val_dataset = TextSamplerDataset(data_val, SEQ_LEN)
